1ebf5e0a0101c0e5e0b1dc12d3046d222dfa7143
Trần Mậu Phi <84634264+tranphi2642@users.noreply.github.com>
2022-09-24 16:09:47 +0700

    update project

--- style/js/index.js
+++ style/js/index.js
@@ -1,32 +1,20 @@
-const body = document.querySelector('body'),
-  modeToggle = body.querySelector('.mode-toggle')
-sidebar = body.querySelector('nav')
-sidebarToggle = body.querySelector('.sidebar-toggle')
+const forms = document.querySelector('.forms'),
+  pwShowHide = document.querySelectorAll('.eye-icon')
 
-let getMode = localStorage.getItem('mode')
-if (getMode && getMode === 'dark') {
-  body.classList.toggle('dark')
-}
+pwShowHide.forEach((eyeIcon) => {
+  eyeIcon.addEventListener('click', () => {
+    let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(
+      '.password',
+    )
 
-let getStatus = localStorage.getItem('status')
-if (getStatus && getStatus === 'close') {
-  sidebar.classList.toggle('close')
-}
-
-modeToggle.addEventListener('click', () => {
-  body.classList.toggle('dark')
-  if (body.classList.contains('dark')) {
-    localStorage.setItem('mode', 'dark')
-  } else {
-    localStorage.setItem('mode', 'light')
-  }
-})
-
-sidebarToggle.addEventListener('click', () => {
-  sidebar.classList.toggle('close')
-  if (sidebar.classList.contains('close')) {
-    localStorage.setItem('status', 'close')
-  } else {
-    localStorage.setItem('status', 'open')
-  }
+    pwFields.forEach((password) => {
+      if (password.type === 'password') {
+        password.type = 'text'
+        eyeIcon.classList.replace('uil-eye-slash', 'uil-eye')
+        return
+      }
+      password.type = 'password'
+      eyeIcon.classList.replace('uil-eye', 'uil-eye-slash')
+    })
+  })
 })
--- style/js/router.js
+++ style/js/router.js
@@ -3,8 +3,5 @@ var app = angular.module('myapp', ['ngRoute'])
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/login', { templateUrl: '/views/login/login.html' })
-    .when('/home', { templateUrl: '/views/home/home.html' })
-    .when('/feedback', { templateUrl: '/views/feedback/feedback.html' })
-    .when('/message', { templateUrl: '/components/message/message.html' })
     .otherwise({ templateUrl: '/views/login/login.html' })
 })
