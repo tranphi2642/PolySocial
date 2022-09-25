c6cc4d7a95050c540bc7f514b3ee33bd4e778900
Trần Mậu Phi <84634264+tranphi2642@users.noreply.github.com>
2022-09-25 20:41:05 +0700

    update project

--- style/js/index.js
+++ style/js/index.js
@@ -1,24 +1,3 @@
-const forms = document.querySelector('.forms'),
-  pwShowHide = document.querySelectorAll('.eye-icon')
-
-pwShowHide.forEach((eyeIcon) => {
-  eyeIcon.addEventListener('click', () => {
-    let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(
-      '.password',
-    )
-
-    pwFields.forEach((password) => {
-      if (password.type === 'password') {
-        password.type = 'text'
-        eyeIcon.classList.replace('uil-eye-slash', 'uil-eye')
-        return
-      }
-      password.type = 'password'
-      eyeIcon.classList.replace('uil-eye', 'uil-eye-slash')
-    })
-  })
-})
-
 const body = document.querySelector('body'),
   modeToggle = body.querySelector('.mode-toggle')
 sidebar = body.querySelector('nav')
--- style/js/router.js
+++ style/js/router.js
@@ -2,6 +2,31 @@ var app = angular.module('myapp', ['ngRoute'])
 
 app.config(function ($routeProvider) {
   $routeProvider
-    .when('/login', { templateUrl: '/views/login/login.html' })
+    .when('/login', {
+      templateUrl: '/views/login/login.html',
+    })
     .otherwise({ templateUrl: '/views/login/login.html' })
+})
+
+app.controller('myCtrl', function ($scope) {
+  window.onload = function () {
+    const pwShowHide = document.querySelectorAll('.eye-icon')
+    pwShowHide.forEach((eyeIcon) => {
+      eyeIcon.addEventListener('click', () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(
+          '.password',
+        )
+
+        pwFields.forEach((password) => {
+          if (password.type === 'password') {
+            password.type = 'text'
+            eyeIcon.classList.replace('uil-eye-slash', 'uil-eye')
+            return
+          }
+          password.type = 'password'
+          eyeIcon.classList.replace('uil-eye', 'uil-eye-slash')
+        })
+      })
+    })
+  }
 })
