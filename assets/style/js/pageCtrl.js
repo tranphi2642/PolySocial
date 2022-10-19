7db7c6206501771190a5088b585b81a6010caf76
Trần Mậu Phi <phitmps16509@fpt.edu.vn>
2022-10-19 22:45:08 +0700

    update project

--- assets/style/js/pageCtrl.js
+++ assets/style/js/pageCtrl.js
@@ -2,6 +2,10 @@ app.controller('pageCtrl', function ($scope) {
   angular.element(document).ready(function () {
     const menuItem = document.querySelectorAll('.menu-item')
 
+    const createQuiz = document.querySelector('#createQuiz')
+    const createModelQuiz = document.querySelector('.modal-create-quiz')
+
+    //active row
     const changeActiveItem = () => {
       menuItem.forEach((item) => {
         item.classList.remove('active')
@@ -14,5 +18,20 @@ app.controller('pageCtrl', function ($scope) {
         item.classList.add('active')
       })
     })
+
+    //create quiz
+    const onpenModel3 = () => {
+      createModelQuiz.style.display = 'grid'
+    }
+
+    createQuiz?.addEventListener('click', onpenModel3)
+
+    const closeModel3 = (e) => {
+      if (e.target.classList.contains('modal-create-quiz')) {
+        createModelQuiz.style.display = 'none'
+      }
+    }
+
+    createModelQuiz?.addEventListener('click', closeModel3)
   })
 })
