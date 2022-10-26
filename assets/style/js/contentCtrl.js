ddc3d461eedd4e3449580046d385988d598528a9
Trần Mậu Phi <phitmps16509@fpt.edu.vn>
2022-10-26 23:22:49 +0700

    update project

--- assets/style/js/contentCtrl.js
+++ assets/style/js/contentCtrl.js
@@ -1,193 +1,193 @@
-app.controller('contentCrtl', function ($scope, $http) {
-  let host = 'http://localhost:8111/post'
+app.controller("contentCrtl", function ($scope, $http) {
+  let host = "http://localhost:8111/post";
 
-  $scope.content = {}
+  $scope.content = {};
 
-  $scope.contents = {}
+  $scope.contents = {};
 
   $scope.findContents = async function (content) {
-    var url = `${host}/find?content=${content}`
+    var url = `${host}/find?content=${content}`;
     await $http
       .get(url)
       .then(function (response) {
-        $scope.contents = response.data
-        alert('success')
+        $scope.contents = response.data;
+        alert("success");
       })
       .catch((error) => {
-        alert('Error' + error)
-      })
-  }
+        alert("Error" + error);
+      });
+  };
 
   $scope.load_All = async function () {
-    var url = `${host}/get/all`
+    var url = `${host}/get/all`;
     await $http
       .get(url)
       .then((response) => {
-        $scope.contents = response.data
-        console.log(response.data)
+        $scope.contents = response.data;
+        console.log(response.data);
       })
       .catch((error) => {
-        alert('Error' + error)
-      })
-  }
+        alert("Error" + error);
+      });
+  };
 
   $scope.edit = async function (id) {
-    const updateContent = document.querySelectorAll('.updateContent')
-    const updateModel2 = document.querySelector('.modal-edit-content')
+    const updateContent = document.querySelectorAll(".updateContent");
+    const updateModel2 = document.querySelector(".modal-edit-content");
 
     const onpenUpdateModel2 = () => {
-      updateModel2.style.display = 'grid'
-    }
+      updateModel2.style.display = "grid";
+    };
 
     updateContent.forEach((ele) =>
-      ele?.addEventListener('click', onpenUpdateModel2),
-    )
+      ele?.addEventListener("click", onpenUpdateModel2)
+    );
 
     const closeUpdateModel2 = (e) => {
-      if (e.target.classList.contains('modal-edit-content')) {
-        updateModel2.style.display = 'none'
+      if (e.target.classList.contains("modal-edit-content")) {
+        updateModel2.style.display = "none";
       }
-    }
+    };
 
-    updateModel2?.addEventListener('click', closeUpdateModel2)
+    updateModel2?.addEventListener("click", closeUpdateModel2);
 
-    var url = `${host}/get/post?postId=${id}`
+    var url = `${host}/get/post?postId=${id}`;
     await $http
       .get(url)
       .then((response) => {
-        $scope.content = response.data
-        $scope.content.createdDate = new Date($scope.content.createdDate)
-        console.log(response.data)
+        $scope.content = response.data;
+        $scope.content.createdDate = new Date($scope.content.createdDate);
+        console.log(response.data);
       })
       .catch((error) => {
-        alert('Error')
-      })
-  }
+        alert("Error");
+      });
+  };
 
   $scope.updateContent = async function () {
-    var st = angular.copy($scope.content)
-    var url = `${host}/update`
+    var st = angular.copy($scope.content);
+    var url = `${host}/update`;
     await $http
       .put(url, st)
       .then((response) => {
-        alert('Success' + response)
-        window.location.reload()
+        alert("Success" + response);
+        window.location.reload();
       })
       .catch((error) => {
-        alert('Error' + error)
-      })
-  }
+        alert("Error" + error);
+      });
+  };
 
   $scope.deleteContent = async function (id) {
-    var url = `${host}/delete?postId=${id}`
+    var url = `${host}/delete?postId=${id}`;
     await $http
       .delete(url)
       .then((response) => {
-        alert('Success' + response)
-        window.location.reload()
+        alert("Success" + response);
+        window.location.reload();
       })
       .catch((error) => {
-        alert('Error' + error)
-      })
-  }
+        alert("Error" + error);
+      });
+  };
 
   $scope.reset = function () {
     $scope.content = {
       postId: $scope.content.postId,
-      content: '',
-      createdDate: '',
-      userId: '',
-      groupId: '',
-    }
-  }
+      content: "",
+      createdDate: "",
+      userId: "",
+      groupId: "",
+    };
+  };
 
   //start
-  $scope.load_All()
-  $scope.reset()
+  $scope.load_All();
+  $scope.reset();
 
   angular.element(document).ready(function () {
-    const tables = document.querySelectorAll('tbody tr')
+    const tables = document.querySelectorAll("tbody tr");
 
-    const createContent = document.querySelector('#createContent')
-    const createModel2 = document.querySelector('.modal-create-content')
+    const createContent = document.querySelector("#createContent");
+    const createModel2 = document.querySelector(".modal-create-content");
 
     //active row
     const removeTableSelector = () => {
       tables.forEach((table) => {
-        table.classList.remove('active-row')
-      })
-    }
+        table.classList.remove("active-row");
+      });
+    };
 
     tables.forEach((table) => {
-      table.addEventListener('click', () => {
-        removeTableSelector()
-        table.classList.add('active-row')
-      })
-    })
+      table.addEventListener("click", () => {
+        removeTableSelector();
+        table.classList.add("active-row");
+      });
+    });
 
     //create content
     const onpenModel2 = () => {
-      createModel2.style.display = 'grid'
-    }
+      createModel2.style.display = "grid";
+    };
 
-    createContent?.addEventListener('click', onpenModel2)
+    createContent?.addEventListener("click", onpenModel2);
 
     const closeModel2 = (e) => {
-      if (e.target.classList.contains('modal-create-content')) {
-        createModel2.style.display = 'none'
+      if (e.target.classList.contains("modal-create-content")) {
+        createModel2.style.display = "none";
       }
-    }
+    };
 
-    createModel2?.addEventListener('click', closeModel2)
-  })
-})
+    createModel2?.addEventListener("click", closeModel2);
+  });
+});
 
-app.controller('create-content', function ($scope, $http) {
-  let host = 'http://localhost:8111/post'
+app.controller("create-content", function ($scope, $http) {
+  let host = "http://localhost:8111/post";
 
-  $scope.content = {}
+  $scope.content = {};
 
-  $scope.contents = {}
+  $scope.contents = {};
 
   $scope.createContent = async function () {
-    var st = angular.copy($scope.content)
-    console.log(st)
-    var url = `${host}/create?userId=${st.userId}&groupId=${st.groupId}`
+    var st = angular.copy($scope.content);
+    console.log(st);
+    var url = `${host}/create?userId=${st.userId}&groupId=${st.groupId}`;
     await $http
       .post(url, st)
       .then((response) => {
-        alert('Success' + response)
-        window.location.reload()
+        alert("Success" + response);
+        window.location.reload();
       })
       .catch((error) => {
-        alert('Error' + error)
-      })
-  }
-})
+        alert("Error" + error);
+      });
+  };
+});
 
-angular.module('myapp').filter('cut', function () {
+angular.module("myapp").filter("cut", function () {
   return function (value, wordwise, max, tail) {
-    if (!value) return ''
+    if (!value) return "";
 
-    max = parseInt(max, 10)
-    if (!max) return value
-    if (value.length <= max) return value
+    max = parseInt(max, 10);
+    if (!max) return value;
+    if (value.length <= max) return value;
 
-    value = value.substr(0, max)
+    value = value.substr(0, max);
     if (wordwise) {
-      var lastspace = value.lastIndexOf(' ')
+      var lastspace = value.lastIndexOf(" ");
       if (lastspace !== -1) {
         //Also remove . and , so its gives a cleaner result.
         if (
-          value.charAt(lastspace - 1) === '.' ||
-          value.charAt(lastspace - 1) === ','
+          value.charAt(lastspace - 1) === "." ||
+          value.charAt(lastspace - 1) === ","
         ) {
-          lastspace = lastspace - 1
+          lastspace = lastspace - 1;
         }
-        value = value.substr(0, lastspace)
+        value = value.substr(0, lastspace);
       }
     }
 
-    return value + (tail || ' …')
-  }
-})
+    return value + (tail || " …");
+  };
+});
--- assets/style/js/homeCtrl.js
+++ assets/style/js/homeCtrl.js
@@ -1,83 +1,99 @@
-app.controller('homeCtrl', function ($scope) {
+app.controller("homeCtrl", function ($scope) {
   angular.element(document).ready(function () {
-    //form home
+    //modal create post
+    const createContent = document.querySelector("#create-post");
+    const createModel = document.querySelector(".modal-create-post");
+
+    const onpenModel = () => {
+      createModel.style.display = "grid";
+    };
+
+    createContent?.addEventListener("click", onpenModel);
+
+    const closeModel = (e) => {
+      if (e.target.classList.contains("modal-create-post")) {
+        createModel.style.display = "none";
+      }
+    };
+
+    createModel?.addEventListener("click", closeModel);
     //sidebar
-    const menuItem = document.querySelectorAll('.menu-item')
+    const menuItem = document.querySelectorAll(".menu-item");
 
     //message
     const messageNotification = document.querySelector(
-      '#messages-notifacations',
-    )
-    const messages = document.querySelector('.messages')
-    const message = messages.querySelectorAll('.message')
-    const messageSearch = document.querySelector('#search-bar')
+      "#messages-notifacations"
+    );
+    const messages = document.querySelector(".messages");
+    const message = messages.querySelectorAll(".message");
+    const messageSearch = document.querySelector("#search-bar");
 
     //theme
-    const theme = document.querySelector('#theme')
-    const themeModel = document.querySelector('.customize-theme')
+    const theme = document.querySelector("#theme");
+    const themeModel = document.querySelector(".customize-theme");
 
     //fontsize
-    const fontSizes = document.querySelectorAll('.choose-size span')
-    var root = document.querySelector(':root')
+    const fontSizes = document.querySelectorAll(".choose-size span");
+    var root = document.querySelector(":root");
 
     //color
-    const colors = document.querySelectorAll('.choose-color span')
+    const colors = document.querySelectorAll(".choose-color span");
 
     //background
-    const bg1 = document.querySelector('.bg-1')
-    const bg2 = document.querySelector('.bg-2')
-    const bg3 = document.querySelector('.bg-3')
+    const bg1 = document.querySelector(".bg-1");
+    const bg2 = document.querySelector(".bg-2");
+    const bg3 = document.querySelector(".bg-3");
 
     //remove active class from all menu items
     const changeActiveItem = () => {
       menuItem.forEach((item) => {
-        item.classList.remove('active')
-      })
-    }
+        item.classList.remove("active");
+      });
+    };
 
     menuItem.forEach((item) => {
-      item.addEventListener('click', () => {
-        changeActiveItem()
-        item.classList.add('active')
-        if (item.id != 'notifications') {
-          document.querySelectorAll('.notifications-popup')[0].style.display =
-            'none'
+      item.addEventListener("click", () => {
+        changeActiveItem();
+        item.classList.add("active");
+        if (item.id != "notifications") {
+          document.querySelectorAll(".notifications-popup")[0].style.display =
+            "none";
         } else {
-          document.querySelectorAll('.notifications-popup')[0].style.display =
-            'block'
+          document.querySelectorAll(".notifications-popup")[0].style.display =
+            "block";
           document.querySelectorAll(
-            '#notifications .notification-count',
-          )[0].style.display = 'none'
+            "#notifications .notification-count"
+          )[0].style.display = "none";
         }
 
-        if (item.id != 'notifications-deline') {
-          document.querySelectorAll('.notifications-deline')[0].style.display =
-            'none'
+        if (item.id != "notifications-deline") {
+          document.querySelectorAll(".notifications-deline")[0].style.display =
+            "none";
         } else {
-          document.querySelectorAll('.notifications-deline')[0].style.display =
-            'block'
+          document.querySelectorAll(".notifications-deline")[0].style.display =
+            "block";
           document.querySelectorAll(
-            '#notifications-deline .notification-count',
-          )[0].style.display = 'none'
+            "#notifications-deline .notification-count"
+          )[0].style.display = "none";
         }
-      })
-    })
+      });
+    });
 
     //message search
 
     const searchMessage = () => {
-      const val = messageSearch.value.toLowerCase()
+      const val = messageSearch.value.toLowerCase();
       message.forEach((chat) => {
-        let name = chat.querySelector('h5').textContent.toLowerCase()
-        if (name.indexOf(val) != -1 || name.indexOf(val) == '') {
-          chat.style.display = 'flex'
+        let name = chat.querySelector("h5").textContent.toLowerCase();
+        if (name.indexOf(val) != -1 || name.indexOf(val) == "") {
+          chat.style.display = "flex";
         } else {
-          chat.style.display = 'none'
+          chat.style.display = "none";
         }
-      })
-    }
+      });
+    };
 
-    messageSearch.addEventListener('keyup', searchMessage)
+    messageSearch.addEventListener("keyup", searchMessage);
 
     // messageNotification.addEventListener('click', () => {
     //   messages.style.boxShadow = '0 0 1rem var(--color-primary)'
@@ -90,128 +106,128 @@ app.controller('homeCtrl', function ($scope) {
 
     //theme
     const onpenThemModel = () => {
-      themeModel.style.display = 'grid'
-    }
+      themeModel.style.display = "grid";
+    };
 
-    theme.addEventListener('click', onpenThemModel)
+    theme.addEventListener("click", onpenThemModel);
 
     const closeThemModel = (e) => {
-      if (e.target.classList.contains('customize-theme')) {
-        themeModel.style.display = 'none'
+      if (e.target.classList.contains("customize-theme")) {
+        themeModel.style.display = "none";
       }
-    }
+    };
 
-    themeModel.addEventListener('click', closeThemModel)
+    themeModel.addEventListener("click", closeThemModel);
 
     //font size
     const removeSizeSelector = () => {
       fontSizes.forEach((size) => {
-        size.classList.remove('active')
-      })
-    }
+        size.classList.remove("active");
+      });
+    };
 
     fontSizes.forEach((size) => {
-      size.addEventListener('click', () => {
-        removeSizeSelector()
-        let fontSize
-        size.classList.toggle('active')
+      size.addEventListener("click", () => {
+        removeSizeSelector();
+        let fontSize;
+        size.classList.toggle("active");
 
-        if (size.classList.contains('font-size-1')) {
-          fontSize = '10px'
-          root.style.setProperty('----sticky-top-left', '5.4rem')
-          root.style.setProperty('----sticky-top-right', '5.4rem')
-        } else if (size.classList.contains('font-size-2')) {
-          fontSize = '13px'
-          root.style.setProperty('----sticky-top-left', '5.4rem')
-          root.style.setProperty('----sticky-top-right', '-7rem')
-        } else if (size.classList.contains('font-size-3')) {
-          fontSize = '16px'
-          root.style.setProperty('----sticky-top-left', '-2rem')
-          root.style.setProperty('----sticky-top-right', '-17rem')
-        } else if (size.classList.contains('font-size-4')) {
-          fontSize = '19px'
-          root.style.setProperty('----sticky-top-left', '-5rem')
-          root.style.setProperty('----sticky-top-right', '-25rem')
-        } else if (size.classList.contains('font-size-5')) {
-          fontSize = '22px'
-          root.style.setProperty('----sticky-top-left', '-12rem')
-          root.style.setProperty('----sticky-top-right', '-35rem')
+        if (size.classList.contains("font-size-1")) {
+          fontSize = "10px";
+          root.style.setProperty("----sticky-top-left", "5.4rem");
+          root.style.setProperty("----sticky-top-right", "5.4rem");
+        } else if (size.classList.contains("font-size-2")) {
+          fontSize = "13px";
+          root.style.setProperty("----sticky-top-left", "5.4rem");
+          root.style.setProperty("----sticky-top-right", "-7rem");
+        } else if (size.classList.contains("font-size-3")) {
+          fontSize = "16px";
+          root.style.setProperty("----sticky-top-left", "-2rem");
+          root.style.setProperty("----sticky-top-right", "-17rem");
+        } else if (size.classList.contains("font-size-4")) {
+          fontSize = "19px";
+          root.style.setProperty("----sticky-top-left", "-5rem");
+          root.style.setProperty("----sticky-top-right", "-25rem");
+        } else if (size.classList.contains("font-size-5")) {
+          fontSize = "22px";
+          root.style.setProperty("----sticky-top-left", "-12rem");
+          root.style.setProperty("----sticky-top-right", "-35rem");
         }
 
-        document.querySelector('html').style.fontSize = fontSize
-      })
-    })
+        document.querySelector("html").style.fontSize = fontSize;
+      });
+    });
 
     //color
     const removeColorSelector = () => {
       colors.forEach((color) => {
-        color.classList.remove('active')
-      })
-    }
+        color.classList.remove("active");
+      });
+    };
 
     colors.forEach((color) => {
-      color.addEventListener('click', () => {
-        removeColorSelector()
-        let primaryHue
-        color.classList.add('active')
+      color.addEventListener("click", () => {
+        removeColorSelector();
+        let primaryHue;
+        color.classList.add("active");
 
-        if (color.classList.contains('color-1')) {
-          primaryHue = 252
-        } else if (color.classList.contains('color-2')) {
-          primaryHue = 52
-        } else if (color.classList.contains('color-3')) {
-          primaryHue = 13
-        } else if (color.classList.contains('color-4')) {
-          primaryHue = 152
-        } else if (color.classList.contains('color-5')) {
-          primaryHue = 202
+        if (color.classList.contains("color-1")) {
+          primaryHue = 252;
+        } else if (color.classList.contains("color-2")) {
+          primaryHue = 52;
+        } else if (color.classList.contains("color-3")) {
+          primaryHue = 13;
+        } else if (color.classList.contains("color-4")) {
+          primaryHue = 152;
+        } else if (color.classList.contains("color-5")) {
+          primaryHue = 202;
         }
 
-        root.style.setProperty('--primary-color-hue', primaryHue)
-      })
-    })
+        root.style.setProperty("--primary-color-hue", primaryHue);
+      });
+    });
 
     //background
-    let ligtColorLightness
-    let darkColorLightness
-    let whiteColorLightness
+    let ligtColorLightness;
+    let darkColorLightness;
+    let whiteColorLightness;
 
     const changeBg = () => {
-      root.style.setProperty('--light-color-lightness', ligtColorLightness)
-      root.style.setProperty('--dark-color-lightness', darkColorLightness)
-      root.style.setProperty('--white-color-lightness', whiteColorLightness)
-    }
+      root.style.setProperty("--light-color-lightness", ligtColorLightness);
+      root.style.setProperty("--dark-color-lightness", darkColorLightness);
+      root.style.setProperty("--white-color-lightness", whiteColorLightness);
+    };
 
-    bg1.addEventListener('click', () => {
-      bg1.classList.add('active')
+    bg1.addEventListener("click", () => {
+      bg1.classList.add("active");
 
-      bg2.classList.remove('active')
-      bg3.classList.remove('active')
-      window.location.reload()
-    })
+      bg2.classList.remove("active");
+      bg3.classList.remove("active");
+      window.location.reload();
+    });
 
-    bg2.addEventListener('click', () => {
-      ligtColorLightness = '15%'
-      darkColorLightness = '95%'
-      whiteColorLightness = '20%'
+    bg2.addEventListener("click", () => {
+      ligtColorLightness = "15%";
+      darkColorLightness = "95%";
+      whiteColorLightness = "20%";
 
-      bg2.classList.add('active')
+      bg2.classList.add("active");
 
-      bg1.classList.remove('active')
-      bg3.classList.remove('active')
-      changeBg()
-    })
+      bg1.classList.remove("active");
+      bg3.classList.remove("active");
+      changeBg();
+    });
 
-    bg3.addEventListener('click', () => {
-      ligtColorLightness = '0%'
-      darkColorLightness = '95%'
-      whiteColorLightness = '10%'
+    bg3.addEventListener("click", () => {
+      ligtColorLightness = "0%";
+      darkColorLightness = "95%";
+      whiteColorLightness = "10%";
 
-      bg3.classList.add('active')
+      bg3.classList.add("active");
 
-      bg1.classList.remove('active')
-      bg2.classList.remove('active')
-      changeBg()
-    })
-  })
-})
+      bg1.classList.remove("active");
+      bg2.classList.remove("active");
+      changeBg();
+    });
+  });
+});
